02f7b8ac27eb16863512dcf47796f9835e652fa4
kelechi <oparaiskelechi@gmail.com>
2022-02-14 13:08:42 +0100

    remove unwanted codes

--- app/src/main/java/com/asterisk/grocerylist/ui/MainActivity.kt
+++ app/src/main/java/com/asterisk/grocerylist/ui/MainActivity.kt
@@ -14,22 +14,12 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
-                    as NavHostFragment
-
-        navController = navHostFragment.navController
-//        setupActionBarWithNavController(navController)
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
-    }
 }
